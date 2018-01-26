28024a3e32f708fdd6256b129e6ebfa660503612
Cory Klein <cory.klein@saucelabs.com>
2018-01-26 12:44:38 -0800

    Add homebrew setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env bash
 
-echo 'Hello world!'
+# Install Homebrew
+if ! brew --version > /dev/null 2>&1; then
+  echo "Installing brew"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "homebrew already installed"
+fi
+
+# Install Homebrew packages
+brew update
+brew install git
+
+
+#git ls-remote "https://github.com/saucelabs/dummy.git" 2> /dev/null
+
+
+# echo "Where would you like code stored? (default: ~/code/)"
+# read INPUT
+# INPUT="${INPUT/#\~/$HOME}"
+# CODE_ROOT=${INPUT:-$HOME/code}
+# echo $CODE_ROOT
+
+# Check if user has access to Github sauce organization
 
