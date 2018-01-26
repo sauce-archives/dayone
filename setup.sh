d80929d145ab7438cc5f0128c5e5a241264891e9
Cory Klein <cory.klein@saucelabs.com>
2018-01-26 12:53:23 -0800

    Don't prompt user for anything

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Install Homebrew
 if ! brew --version > /dev/null 2>&1; then
   echo "Installing brew"
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
 else
   echo "homebrew already installed"
 fi
